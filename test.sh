2e2784e076ac675494b285d2e6b65c8e389ae6e3
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-03-12 03:15:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,4 @@
-#ATTENTION:
-#First argument: program name without extension ".exe"
-#Second argument: Number of input output pairs in the format of (input/output)(First argument)_(num).txt in which num is from 1 to second argoument inclusively
-#For example: first parameter=dog.exe , input file 1 must be named "inputdog_1.txt", output file 1 must be named "outputdog_1.txt"
-#Third argument: The name of folder that includes the test cases and output
-#The program must have postfix of .exe and must be put under the current path, and the test cases and output must be put under the path in third argument
-#The output will be echo'd in console
+
 for((i=1;i<=$2;i++))
 do
   if [ -e $3/input$1_$i.txt ]
